81c5c6e9f3e06dc7390644594587c66b5cd01086
Christiaan Bloemendaal <github@tnrd.net>
2020-09-02 12:54:45 +0200

    Added models

---
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Models/Platform.cs
@@ -0,0 +1,9 @@
+namespace FreeGameIsAFreeGame.Core.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public string Identifier { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
