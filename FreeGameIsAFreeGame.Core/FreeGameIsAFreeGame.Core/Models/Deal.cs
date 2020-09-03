df45ef140767e9c44d0e279db827943ba3a9f8fd
Christiaan Bloemendaal <github@tnrd.net>
2020-09-03 21:42:03 +0200

    Updated Deal.Platform to PlatformId

--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Models/Deal.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Models/Deal.cs
@@ -5,7 +5,7 @@ namespace FreeGameIsAFreeGame.Core.Models
     public class Deal
     {
         public int Id { get; set; }
-        public Platform Platform { get; set; }
+        public int PlatformId { get; set; }
         public string Title { get; set; }
         public string Link { get; set; }
         public string Image { get; set; }
