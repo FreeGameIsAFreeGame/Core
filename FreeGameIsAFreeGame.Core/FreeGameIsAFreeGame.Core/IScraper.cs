6a96bfdff14173a592f6f7ebc4e5e7ea888669db
Christiaan Bloemendaal <github@tnrd.net>
2020-09-04 12:37:23 +0200

    Added IScraper interface

---
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/IScraper.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FreeGameIsAFreeGame.Core.Models;
+
+namespace FreeGameIsAFreeGame.Core
+{
+    public interface IScraper
+    {
+        string Identifier { get; }
+        Task<IEnumerable<IDeal>> Scrape(CancellationToken token);
+    }
+}
