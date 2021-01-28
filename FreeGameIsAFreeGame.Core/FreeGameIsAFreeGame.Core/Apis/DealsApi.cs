2ad2837b3ab24beb4387360b979ca5f4de849aa0
Christiaan Bloemendaal <github@tnrd.net>
2021-01-28 20:27:43 +0100

    ### Added - Extra api calls in Deals and Platforms api

--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DealsApi.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DealsApi.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FreeGameIsAFreeGame.Core.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace FreeGameIsAFreeGame.Core.Apis
 {
@@ -8,5 +12,31 @@ namespace FreeGameIsAFreeGame.Core.Apis
         /// <inheritdoc />
         protected override string Slug => "deals";
 #endregion
+
+        public async Task<IEnumerable<IDeal>> GetByPlatform(int id)
+        {
+            RestRequest request = new RestRequest($"api/{Slug}/platform/{id}", Method.GET);
+            IRestResponse response = await Api.Client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<Deal>>(response.Content);
+            }
+
+            throw new ApiException(response);
+        }
+
+        public async Task<IDeal> Patch(IDeal deal)
+        {
+            RestRequest request = new RestRequest($"api/{Slug}", Method.PATCH);
+            request.AddJsonBody(deal);
+
+            IRestResponse response = await Api.Client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<Deal>(response.Content);
+            }
+
+            throw new ApiException(response);
+        }
     }
 }
--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/PlatformsApi.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/PlatformsApi.cs
@@ -1,4 +1,8 @@
+using System.Net;
+using System.Threading.Tasks;
 using FreeGameIsAFreeGame.Core.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace FreeGameIsAFreeGame.Core.Apis
 {
@@ -8,5 +12,36 @@ namespace FreeGameIsAFreeGame.Core.Apis
         /// <inheritdoc />
         protected override string Slug => "Platforms";
 #endregion
+
+        public async Task<IPlatform> GetByIdentifier(string identifier)
+        {
+            IRestRequest request = new RestRequest($"api/{Slug}/{identifier}", Method.GET);
+            IRestResponse result = await Api.Client.ExecuteAsync(request);
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<Platform>(result.Content);
+            }
+
+            if (result.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+            throw new ApiException(result);
+        }
+
+        public async Task<IPlatform> Patch(IPlatform deal)
+        {
+            RestRequest request = new RestRequest($"api/{Slug}", Method.PATCH);
+            request.AddJsonBody(deal);
+
+            IRestResponse response = await Api.Client.ExecuteAsync(request);
+            if (response.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<Platform>(response.Content);
+            }
+
+            throw new ApiException(response);
+        }
     }
 }
