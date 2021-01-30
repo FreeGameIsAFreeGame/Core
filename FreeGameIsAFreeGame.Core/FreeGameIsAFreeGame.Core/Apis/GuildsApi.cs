8c4218ac86473b555d4450e032ba30d56ce7da47
Christiaan Bloemendaal <github@tnrd.net>
2021-01-30 22:37:23 +0100

    ### Added - GetBySnowflake method for Guilds api

--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/GuildsApi.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/GuildsApi.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using System.Threading.Tasks;
 using FreeGameIsAFreeGame.Core.Models;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace FreeGameIsAFreeGame.Core.Apis
@@ -10,6 +12,23 @@ namespace FreeGameIsAFreeGame.Core.Apis
         /// <inheritdoc />
         protected override string Slug => "guilds";
 #endregion
+
+        public async Task<IGuild> GetBySnowflake(ulong snowflake)
+        {
+            IRestRequest request = new RestRequest($"api/{Slug}/snowflake/{snowflake}", Method.GET);
+            IRestResponse result = await Api.Client.ExecuteAsync(request);
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<Guild>(result.Content);
+            }
+
+            if (result.StatusCode == HttpStatusCode.NotFound)
+            {
+                return default;
+            }
+
+            throw new ApiException(result);
+        }
 
         public async Task<bool> Delete(IGuild guild)
         {
