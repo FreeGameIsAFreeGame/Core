5f6920fbfa696a4dfd4974ebea33370a5b1f0476
Christiaan Bloemendaal <github@tnrd.net>
2021-01-30 19:59:14 +0100

    ### Added - Extra functions to match api
    ### Changed
    - Version bump

--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DiscordPostedDealsApi.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DiscordPostedDealsApi.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FreeGameIsAFreeGame.Core.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace FreeGameIsAFreeGame.Core.Apis
 {
@@ -8,5 +12,17 @@ namespace FreeGameIsAFreeGame.Core.Apis
         /// <inheritdoc />
         protected override string Slug => "discord/deals";
 #endregion
+
+        public async Task<IEnumerable<IDiscordPostedDeal>> GetForDeal(IDeal deal)
+        {
+            IRestRequest request = new RestRequest($"api/{Slug}/deal/{deal.Id}", Method.GET);
+            IRestResponse result = await Api.Client.ExecuteAsync(request);
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<IDiscordPostedDeal>>(result.Content);
+            }
+
+            throw new ApiException(result);
+        }
     }
 }
--- FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DiscordPostedRemindersApi.cs
+++ FreeGameIsAFreeGame.Core/FreeGameIsAFreeGame.Core/Apis/DiscordPostedRemindersApi.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using FreeGameIsAFreeGame.Core.Models;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace FreeGameIsAFreeGame.Core.Apis
 {
@@ -8,5 +12,17 @@ namespace FreeGameIsAFreeGame.Core.Apis
         /// <inheritdoc />
         protected override string Slug => "discord/reminders";
 #endregion
+
+        public async Task<IEnumerable<IDiscordPostedReminder>> GetForDeal(IDeal deal)
+        {
+            IRestRequest request = new RestRequest($"api/{Slug}/deal/{deal.Id}", Method.GET);
+            IRestResponse result = await Api.Client.ExecuteAsync(request);
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<IDiscordPostedReminder>>(result.Content);
+            }
+
+            throw new ApiException(result);
+        }
     }
 }
